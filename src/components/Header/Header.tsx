157439820e0cf3032432979692ab9efabb5a5ea8
Zeliwipin <zeliwipin@gmail.com>
2022-03-24 09:39:36 +0100

    added the header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="h-[72px] bg-[#222222] flex items-center justify-center text-xl">
+      <div className="text-[#dddddd] uppercase">Wiki Math and Computer</div>
+    </header>
+  );
+};
